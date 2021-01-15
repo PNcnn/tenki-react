23bfea703beb05bb8d2283ac593a4cd949c2265a
PNcnn <pncnn314@gmail.com>
2021-01-15 12:57:28 +0900

    redux temp

--- src/index.js
+++ src/index.js
@@ -1,57 +1,110 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-class NameForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: '',
-      weather: [],
-      data: ''
-    };
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import { Provider, connect } from 'react-redux'
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+//Presentational Components
+class AppComponents extends React.Component {
 
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-
-  handleSubmit(event) {
-    const appid =''
-    const url = 'https://api.openweathermap.org/data/2.5/weather?q='+ this.state.value +'&appid='+ appid
-    var obj = []
-    fetch(url)
-      .then(response => response.json())
-      .then(data =>
-        this.setState({
-          weather: data
-        })
-      );
-      console.log(this.state.weather.weather)
-      if(this.state.weather.weather){
-        obj = this.state.weather.weather[0].description
-        console.log(obj)
-        this.setState({data: obj})
-      }
-      event.preventDefault();
+  send(e){
+    this.props.onClick(this.refs.inputText.value);
   }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Place:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
-        </label>
-        <input type="submit" value="Weather" />
-        <br />({this.state.data}){this.state.value}
-      </form>
+      <div>
+        <input type="text" defaultValue="" ref="inputText" />
+        <button onClick={this.send.bind(this)}>計算</button>
+        <br />
+        {this.props.price}<br />
+        {this.props.weather}
+      </div>
     );
   }
 }
 
+function mapStateToProps(state) {
+  return {
+    price: state.price,
+    data: state.data,
+    weather: state.weather
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    onClick(price,data,weather){
+      dispatch(addTax(price,data,weather));
+    }
+  };
+}
+
+let AppContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(AppComponents);
+
+
+
+// ActionCreator
+const ADDTAX = 'ADDTAX';
+function addTax(price,data,weather) {
+  return {
+    type: ADDTAX,
+    price,
+    data,
+    weather
+  };
+}
+
+
+// Reducer
+function appReducer(state, action) {
+  switch (action.type) {
+    case 'ADDTAX':
+      const appid ='f48d28e89819515c1b2f219c5bb5bca4'
+      const url = 'https://api.openweathermap.org/data/2.5/weather?q='+ action.price +'&appid='+ appid
+      fetch(url)
+      .then(response => response.json())
+      .then(data =>
+        state.data = data
+      );
+
+      state.price = action.price
+
+      if(state.data.weather){
+        console.log(state.data.weather[0].description)
+        state.weather = state.data.weather[0].description
+      }
+
+      console.log(state)
+
+      return {
+        data: state.data,
+        price: state.price,
+        weather: state.weather
+      }
+
+    default:
+      return state
+  }
+}
+
+
+//state初期化
+const initialState = {
+  price: '',
+  data: [],
+  weather: ''
+};
+
+//store作成
+const store = createStore(appReducer, initialState);
+
+//レンダリング
 ReactDOM.render(
-  <NameForm />,
+  <Provider store={store}>
+    <AppContainer />
+  </Provider>,
   document.getElementById('root')
 );
